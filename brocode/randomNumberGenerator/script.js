3ae96ba51b0b0f8541657579f5c4c36090aafb1e
Kaustubh Gaur <kaustubhgaur.work@gmail.com>
2026-05-28 16:36:34 +0530

    Made random number generator with range

--- brocode/randomNumberGenerator/script.js
+++ brocode/randomNumberGenerator/script.js
@@ -1,10 +1,15 @@
 const numlabel = document.getElementById("num");
-const generate = document.getElementById("btn1");
-let msg = document.getElementById("msg");
+const generate = document.getElementById("submit");
+const max = document.getElementById("max");
+const min = document.getElementById("min");
 
 let num = 0;
+
 generate.onclick = function () {
-  num = Math.floor(Math.random() * 6 + 1);
+  let maxValue = Number(max.value);
+  let minValue = Number(min.value);
+
+  num = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+
   numlabel.textContent = num;
-  msg.textContent = "Die is rolled";
 };
