7979cc2118e2415e833a7072a5f46d52fc9bbf34
Kaustubh Gaur <kaustubhgaur.work@gmail.com>
2026-08-13 05:13:15 +0530

    create CRUD APIs throught express and tested through postman

--- backend-learning/src/app.js
+++ backend-learning/src/app.js
@@ -17,4 +17,23 @@ app.get("/notes", (req, res) => {
   });
 });
 
+app.delete("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  delete notes[index];
+  res.status(200).json({
+    message: "note deleted sucessfully",
+  });
+});
+
+app.patch("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  const description = req.body.description;
+
+  notes[index].description = description;
+
+  res.status(200).json({
+    message: "description added sucessfully",
+  });
+});
+
 module.exports = app;
