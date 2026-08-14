5f18e8e0041cf04fe5e608a62abf300a754307fc
Kaustubh Gaur <kaustubhgaur.work@gmail.com>
2026-08-14 07:47:00 +0530

    create model using mongoose

--- backend-learning/learning2/src/app.js
+++ backend-learning/learning2/src/app.js
@@ -1,5 +1,28 @@
 const express = require("express");
+const noteModel = require("./models/note.model");
 
 const app = express();
+app.use(express.json());
+
+/*
+CRUD operation:
+
+Create : POST
+Read :   GET
+Update : PATCH
+Delete : DELETE
+
+*/
+
+app.post("/notes", async (req, res) => {
+  const data = req.body;
+  await noteModel.create({
+    title: data.title,
+    description: data.description,
+  });
+  res.status(201).json({
+    message: "Note created successfully.",
+  });
+});
 
 module.exports = app;
