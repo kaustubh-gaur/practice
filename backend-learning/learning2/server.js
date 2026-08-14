5c2fe3d3a02b5b7b18deb648b5ccd6a1cca3ccef
Kaustubh Gaur <kaustubhgaur.work@gmail.com>
2026-08-14 06:49:56 +0530

    connected db

--- backend-learning/learning2/server.js
+++ backend-learning/learning2/server.js
@@ -1,4 +1,7 @@
 const app = require("./src/app");
+const connectDB = require("./src/db/db");
+
+connectDB();
 
 app.listen("3000", (req, res) => {
   console.log("Server is running on port 3000");
--- backend-learning/learning2/src/db/db.js
+++ backend-learning/learning2/src/db/db.js
@@ -1,1 +1,17 @@
 const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const dns = require("dns");
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+async function connectDB() {
+  await mongoose.connect(process.env.MONGODB_URI, {
+    dbname: "halley",
+  });
+
+  console.log("Connected to DB");
+}
+
+module.exports = connectDB;
